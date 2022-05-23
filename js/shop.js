5526c744b248097b2078a63e51a364996ba89457
Kalinin Konstantin <kostya040601@gmail.com>
2022-05-23 14:16:22 +0300

    qwe

--- js/shop.js
+++ js/shop.js
@@ -6,8 +6,10 @@ function loadGoods(){
     $.getJSON('db.json', function(data){
         var out = '';
         for(var key in data){
-            out+='<img src="'+data[key].image+'" alt="Nevermind T-Shirt">';
-            out+='<p>'+data[key]['name']+'<p>';
+            out+='<div class="'+data[key].key+'">';
+            out+='<img src="'+data[key].image+'" alt="T-Shirt">';
+            out+='<h2>'+data[key]['name']+'<h2>';
+            out+='<p>'+data[key]['price']+'<p>';
         }
         $('#goods').html(out);
     })
