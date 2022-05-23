fceeb7802d31db1cabd5f45d67b0942468511b2d
Kalinin Konstantin <kostya040601@gmail.com>
2022-05-23 20:18:27 +0300

    cart

--- js/cart.js
+++ js/cart.js
@@ -8,7 +8,15 @@ $.getJSON('db.json', function(data){
     function showCart(){
         var out ='';
         for(var key in cart){
-            out += key + '<br>';
+            out += '<div class="single-good-in-cart">';
+            out += '<button class="delete-button">X</button>';
+            out += '<img class="good-img" src="'+db[key].image+'" alt="single-good">';
+            out += goods[key].name;
+            out += '<button class="minus-button">-</button>';
+            out += cart[key];
+            out += '<button class="plus-button">+</button>';
+            out += cart[key]*goods[key].price;
+            out += '</div>';
         }
         $('#my-cart').html(out);
     }
