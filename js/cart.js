1b4bc23061ff21a31f48e9a022400b9cd809d58d
Kalinin Konstantin <kostya040601@gmail.com>
2022-05-24 11:05:41 +0300

    cart-buttons

--- js/cart.js
+++ js/cart.js
@@ -9,18 +9,47 @@ $.getJSON('db.json', function(data){
         var out ='';
         for(var key in cart){
             out += '<div class="single-good-in-cart">';
-            out += '<button class="delete-button"><img src="img/buttons/delete.png" alt="delete"></button>';
+            out += '<button class="delete-button" data-art="'+ key +'"><img src="img/buttons/delete.png" alt="delete"></button>';
             out += '<img src="'+goods[key].image+'" alt="single-good-in-cart">';
             out += '<h2>'+ goods[key].name +'</h2>';
             out += '<ul class="amount-of-goods">';
-            out += '<li><button class="minus-button"><img src="img/buttons/minus.png" alt="minus"></button></li>';
+            out += '<li><button class="minus-button" data-art="'+ key +'"><img src="img/buttons/minus.png" alt="minus"></button></li>';
             out += '<li><h3>'+ cart[key] +'</h3></li>';
-            out += '<li><button class="plus-button"><img src="img/buttons/plus.png" alt="plus"></button></li>';
+            out += '<li><button class="plus-button" data-art="'+ key +'"><img src="img/buttons/plus.png" alt="plus"></button></li>';
             out += '</ul>';
             out += '<p>Цена: $'+ cart[key]*goods[key].price +'</p>';
             out += '</div>';
         }
         $('#my-cart').html(out);
+        $('.plus-button').on('click', plusGoods);
+        $('.minus-button').on('click', minusGoods);
+        $('.delete-button').on('click', deleteGoods);
+    }
+
+    function plusGoods(){
+        var articul = $(this).attr('data-art');
+        cart[articul]++;
+        saveCartToLS();
+        showCart();
+    }
+
+    function minusGoods(){
+        var articul = $(this).attr('data-art');
+        if(cart[articul] > 1){
+            cart[articul]--;
+        }
+        else{
+            delete cart[articul];
+        }
+        saveCartToLS();
+        showCart();
+    }
+
+    function deleteGoods(){
+        var articul = $(this).attr('data-art');
+        delete cart[articul];
+        saveCartToLS();
+        showCart();
     }
 });
 
@@ -28,4 +57,8 @@ function checkCart(){
     if(localStorage.getItem('cart') != null){
         cart = JSON.parse(localStorage.getItem('cart'));
     }
+}
+
+function saveCartToLS(){
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
