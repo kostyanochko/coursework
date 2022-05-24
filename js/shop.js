bdf5d1ffcdfc513be2b9878e147e1e5027169cf6
Kalinin Konstantin <kostya040601@gmail.com>
2022-05-24 23:39:44 +0300

    qqq

--- js/shop.js
+++ js/shop.js
@@ -10,13 +10,14 @@ function loadGoods(){
     $.getJSON('db.json', function(data){
         var out = '';
         for(var key in data){
-            console.log(typeof(data[key].category))
-            out+='<div class="single-goods">';
-            out+='<img class="goods-img" src="'+data[key].image+'" alt="T-Shirt" data-art="'+key+'">';
-            out+='<h2>'+data[key]['name']+'<h2>';
-            out+='<p>$'+data[key]['price']+'<p>';
-            out+='<p class="in-cart" data-art="'+key+'">В КОРЗИНУ</p>';
-            out+='</div>';
+            if(key === 'shirt1'){
+                out+='<div class="single-goods">';
+                out+='<img class="goods-img" src="'+data[key].image+'" alt="T-Shirt" data-art="'+key+'">';
+                out+='<h2>'+data[key]['name']+'<h2>';
+                out+='<p>$'+data[key]['price']+'<p>';
+                out+='<p class="in-cart" data-art="'+key+'">В КОРЗИНУ</p>';
+                out+='</div>';
+            }
         }
         $('#goods').html(out);
         $('img.goods-img').on('click', addToCart);
